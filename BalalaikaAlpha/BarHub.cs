f87f7698543159c639b3e3b86c2e860f8db5f36d
Anton Mitrofanov <milording@gmail.com>
2017-12-15 17:16:30 +0300

    Add project files.

---
+++ BalalaikaAlpha/BarHub.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Microsoft.AspNet.SignalR;
+
+namespace BalalaikaAlpha
+{
+    public class BarHub : Hub
+    {
+        public string Hello(string name)
+        {
+            return "Hey " + name;
+        }
+    }
+}
